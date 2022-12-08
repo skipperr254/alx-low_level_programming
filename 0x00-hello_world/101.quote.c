8b958c58063a62ae78c6a887c047948047490050
skipperr254 <109298318+skipperr254@users.noreply.github.com>
2022-12-08 23:04:40 +0300

    Update 101.quote.c

--- 0x00-hello_world/101.quote.c
+++ 0x00-hello_world/101.quote.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <unistd.h>
 
 /**
  *  * main - write to stdout
  *   * Return: Always 0
- *    */
+ *    
+*/
 int main(void)
 {
 		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
