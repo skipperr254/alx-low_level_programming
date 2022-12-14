cf38fb0e912c64e8c4c21a216ba17a8d05007674
skipperr254 <109298318+skipperr254@users.noreply.github.com>
2022-12-14 15:50:51 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  prints _putchar, followed by new linees
+ */
+int main(void)
+{
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
