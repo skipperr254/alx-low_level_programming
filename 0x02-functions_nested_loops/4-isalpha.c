f41558dc708954f1f9bbd73fcb0ef56266874ce7
skipperr254 <109298318+skipperr254@users.noreply.github.com>
2022-12-14 16:02:06 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
