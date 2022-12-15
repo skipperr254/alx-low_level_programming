ce36aee4ae19125b0a667a18556ce4377d44c125
Peter Kanini <skipperr254@gmail.com>
2022-12-14 23:25:13 -0800

    Added main.h and 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Tests if function is positive or negativr
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
