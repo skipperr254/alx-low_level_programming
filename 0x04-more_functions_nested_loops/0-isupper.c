a5a3efd1061b34d4d0e42117e05451530833bd6a
skipperr254 <109298318+skipperr254@users.noreply.github.com>
2022-12-16 13:40:09 +0300

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
 /*
- * main - checks whether a character is upper
- *
+ * _isupper - checks whether a character is upper
+ * @c - Char to be checked for case
+ * Return: 0 when true else false
  */
 
-int main(int c) {
+int _isupper(int c) {
+    if (char >= 'A' && 'Z') {
+        return (1);
+    }
+    return (0);
 }
