0ae6882cba270a08334fb1c4de38df2d27c27307
Peter Kanini <skipperr254@gmail.com>
2023-01-05 11:57:40 -0800

    Str lengtj

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of a string
+ * @s: string to find length
+ * Return: length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
