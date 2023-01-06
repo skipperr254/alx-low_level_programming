db7ff6314c85c1b980bb851967dc224358eeb0a8
Peter Kanini <skipperr254@gmail.com>
2023-01-06 00:16:38 -0800

    Prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "maun.h"
+
+/**
+ * is_prime_number - check for prime
+ * @n: number to check
+ * Return: 0 or 1
+ */
+
+int is_prime_number(int n)
+{
+	return (_is_prime_number(int n, 2));
+}
+
+/**
+ * _is_prime_number - finds out whether a number is prime or not
+ * @n: number which to chexk for prime
+ * @i: number from where to check
+ * Return: 0 or 1
+ */
+
+int _is_prime_number(int n, int i)
+{
+	if (n < 2)
+		return (0);
+	if (n == 2 || n == 3)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	if (i == n / 2)
+		return (1);
+	return (_is_prime_number(n, i + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
+int is_prime_number(int n);
+int _is_prime_number(int n, int i);
 
 
 #endif
