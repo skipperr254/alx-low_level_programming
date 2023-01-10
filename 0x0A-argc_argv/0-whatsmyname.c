902143d85417422bea5d7b274e1b442725c40402
Peter Kanini <skipperr254@gmail.com>
2023-01-10 01:53:39 -0800

    corrected whats my name file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,10 @@
  * main - prints current prigram name
  * @argc: number of args
  * @argv: arguments
+ * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
