636334f9123c8a898537a6d6964265524cbddc9b
Peter Kanini <skipperr254@gmail.com>
2023-01-10 01:49:23 -0800

    print current filename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints current prigram name
+ * @argc: number of args
+ * @argv: arguments
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
