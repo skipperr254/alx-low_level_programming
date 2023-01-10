b3aac8806f1a94c2a490f5b82804b4cd39998a54
Peter Kanini <skipperr254@gmail.com>
2023-01-10 01:59:07 -0800

    print number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number os args
+ * @argc: number of args
+ * @argv: args
+ * Return: always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)))
+{
+	printf("%d", argc);
+	return (0);
+}
