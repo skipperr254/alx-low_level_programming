008bf127d68b7dc78929852fe285f2116f19f0d6
Peter Kanini <skipperr254@gmail.com>
2023-01-10 08:53:19 -0800

    bug fixes

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,12 +11,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc > 1)
+	for (i = 1; i < argc; i++)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
