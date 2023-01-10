5bfa03b86f46aea5c8363a037f9ec1709d54d737
Peter Kanini <skipperr254@gmail.com>
2023-01-10 10:49:35 -0800

    mul args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints product of numbers
+ * @argc: number of args
+ * @argv: args
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 3)
+	{
+		int res;
+
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+		return (0);
+	}
+	printf("ERROR\n");
+	return (1);
+}
