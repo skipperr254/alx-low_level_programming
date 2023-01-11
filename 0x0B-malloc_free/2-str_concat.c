9a28e8a1c966f3b9e8d98526b43f64ba81c0744a
Peter Kanini <skipperr254@gmail.com>
2023-01-11 05:23:27 -0800

    edit 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,6 +22,12 @@ char *str_concat(char *s1, char *s2)
 	if (str == NULL)
 		return (NULL);
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		*(str + i) = s1[i];
 	for (j = 0; s2[j] != '\0'; j++)
